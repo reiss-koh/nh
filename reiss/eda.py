df647114a2cf180a17a92164f234236d58dc7a02
reiss-koh <reiss.koh@gmail.com>
2022-11-04 11:24:40 +0900

    update

---
+++ reiss/eda.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+from numpy import nan
+from scipy import stats
+from datetime import date, datetime
+import investpy
+from pandas_datareader import data as pdr
+from pandas_profiling import ProfileReport
+
+df = pd.read_csv("GA.csv", encoding='utf-8', delimiter='|')
+profile = ProfileReport(df, title="Report")
+profile.to_file("GA.html")
